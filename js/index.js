54cc5189a9eb97666f44489cfdb2d8e2ce5ca345
Alex Fallenstedt <a.fallenstedt@gmail.com>
2017-06-23 15:12:15 -0700

    example of how to use dem sperks and smerks

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 // Code will go here, someday!  Someday I will grow up to be an adult softwares!
 
 //debug while I figure out particles
-require('./particles')
+const Sparks = require('./sparks')
+const Smoke = require('./smoke')
 
 require('./fidget-spinner')
+
+Sparks.setup();
+Smoke.setup();
+Sparks.playSparks();
+Smoke.playSmoke();
